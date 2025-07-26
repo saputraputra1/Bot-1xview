bb5758782caba0441940cdebdfea88d076a26271
saputraputra1 <bungadewi12345@gmail.com>
2025-07-26 11:27:54 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+{
+  "botSettings": {
+    "prefix": "!xview",
+    "adminNumber": "6281234567890@c.us",
+    "maxStorageDays": 7,
+    "autoDelete": true,
+    "notifyAdmin": true
+  },
+  "mediaSettings": {
+    "savePath": "./saved_media",
+    "allowedTypes": ["image", "video"],
+    "maxFileSizeMB": 10,
+    "compressImages": true
+  },
+  "messages": {
+    "welcome": "👋 Bot View Once siap membantu! Ketik !xview untuk bantuan",
+    "help": "📌 *Bantuan Bot*\n\n!xview - Tampilkan menu bantuan\n!xview save - Simpan media\n!xview list - Lihat daftar media",
+    "mediaSaved": "✅ Media disimpan sebagai: {filename}",
+    "mediaDetected": "📌 Media terdeteksi! Ketik !xview save untuk menyimpan"
+  }
+}
